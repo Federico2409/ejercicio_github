d24ef51ad54590c121b4720f38a45b40a9e8bb77
giampiero0 <giampiero2427@gmail.com>
2023-09-20 15:07:20 -0500

    ejercicio git

---
+++ ejercicio_git/src/main/java/com/mycompany/ejercicio_git/Ejercicio_git.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_git;
+
+/**
+ *
+ * @author giamp
+ */
+public class Ejercicio_git {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
